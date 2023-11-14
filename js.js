3f0291baba10c30b38cad7c55a5fcd59062d94e1
Vincenzo Tito <vincenzotito@icloud.com>
2023-11-14 20:26:59 +0100

    salvataggio

---
+++ js.js
@@ -0,0 +1,96 @@
+//  14/11 esercizio n.1 //
+
+//let merceMagazzino = 30;
+//let quantitàOrdine = parseInt(prompt("Benvenuto nel nostro Store\n\n Inserisci qui la quantità richiesta"));
+
+
+//if (quantitàOrdine<=30){
+    //alert("Il tuo ordine è andato a buon fine")
+//} 
+
+
+//else if (quantitàOrdine>30) {
+    //alert("Siamo spiacenti la quantità richiesta non è al momnto disponibile")
+//}
+
+
+
+// esercizio n.4 //
+
+/*let array_1 = [
+    ['un', 'per','incatenarli'],
+    ['Anello', 'trovarli'],
+    ['ghermirli','e'],
+    ['gondor','mark'],
+];
+
+let array_2 = [
+    [['trovarli']],
+    ['tu', 'sciocchi'],
+    ['tu', 'sciocchi', ['padron','Sauron']],
+    ['nel',['fuggite', 'gandalf']],
+    [['domarli', 'passare'], 'buio']
+];*/
+
+/*console.log(` ${array_1[0][0]}  ${array_1[1][0]}  ${array_1[0][1]}  ${array_2[4][0][0]}  ${array_1[0][0]}  ${array_1[1][0]}  ${array_1[0][1]}  ${array_2[0][0][0]}  ${array_1[0][0]}  ${array_1[1][0]} ${array_1[0][1]}  ${array_1[2][0]} ${array_1[2][1]}  ${array_2[3][0]}  ${array_2[4][1]} ${array_1[0][2]}`);*/
+
+
+
+// esercizio n.2 //
+
+/*let v = 30;
+
+        if(v<18) {
+            console.log("insufficiente");
+        } else if(v>=18 && v<21) {
+            console.log("sufficiente");
+        }
+        else if(v>=21 && v<24) {
+            console.log("buono");
+        }
+        else if(v>=24 && v<27) {
+            console.log("distinto");
+        }
+        else if(v>=27 && v<29) {        
+            console.log("ottimo");
+        }
+        else if(v=30) {
+            console.log("eccellente");
+        }
+        else{
+            console.log("conswgnato in bianco");
+        }*/
+
+
+        //esercizio extra//
+
+        /*let selezioneBibite = parseInt(prompt("Premi 1 per CocaCola \n\n Premi 2 per Fanta\n\n Premi 3 per Estathe\n\n"))
+
+        switch(selezioneBibite){
+            
+            case 1:
+                if(selezioneBibite == 1){
+                    alert("ecco la tua CocaCola")
+                }
+
+            break;
+
+            case 2:
+                if(selezioneBibite == 2){
+                    alert("ecco la tua Fanta");2
+                }
+
+            break;
+
+            case 3:
+                if(selezioneBibite == 3){
+                    alert("ecco la tua Estathe");
+                }
+
+            break;
+
+            default:
+                alert("Prodotti non disponibili")
+        }*/
+
+        
